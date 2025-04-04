234810a6afdeb169a5d4494381913f0ebb571c8b
Efoe Max <efoemaxi17@gmail.com>
2025-04-04 23:34:04 +0100

    learning tomorrow how to search i.e filter and implement on site

--- Files/find.js
+++ Files/find.js
@@ -1,14 +1,2 @@
 "use script";
 
-let section = document.querySelector(".jobs");
-let element="";
-for(let i = 0; i < array.length; i++){
-    element = `<div>
-    <h2>${title.value}</h2>
-    <h3>Company Name: ${cname.value}</h3>
-    <h3>Type: ${type.value}</h3>
-    <h3>Salary: ${salary.value}</h3></div><br>
-    `; 
-
-    section.innerHTML += element;
-}
